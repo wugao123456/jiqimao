929f052dd849c66ec00642e65381d6159298caf1
wugao123456 <1248991348@qq.com>
2018-04-13 20:42:20 +0800

    初始化所有代码
    初始化所有代码

---
+++ weixin/Senparc.Weixin.MP/AdvancedAPIs/MerChant/Picture/PictureResult.cs
@@ -0,0 +1,17 @@
+﻿//using Senparc.Weixin.Entities;
+using Senparc.Weixin.MP.AppStore;
+
+namespace Senparc.Weixin.MP.AdvancedAPIs.MerChant
+{
+    /// <summary>
+    /// 上传图片返回结果
+    /// </summary>
+    public class PictureResult : WxJsonResult
+    {
+        /// <summary>
+        /// 图片Url
+        /// </summary>
+        public string image_url { get; set; }
+    }
+}
+
